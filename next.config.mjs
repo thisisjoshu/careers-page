1aec9ae8042bb611c784c204332a967af857a934
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2024-08-26 21:46:43 +1100

    tryhing yet again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/careers-page",
   basePath: "/careers-page",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
